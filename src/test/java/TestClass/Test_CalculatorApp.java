994d974fbf5d45cb6d6680b3d96fdaa1ca53397c
ShreyasBhagat <shreyasbhagat98@gmail.com>
2025-06-01 23:51:43 +0530

    Added the Calculator app.

---
+++ src/test/java/TestClass/Test_CalculatorApp.java
@@ -0,0 +1,39 @@
+/**
+* Author: Shreyas Bhagat
+* Date: 1 Jun 2025
+* Description: 
+*/
+package TestClass;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import pomClass.Calculator;
+
+public class Test_CalculatorApp {
+	Calculator calc = new Calculator();
+	@Test
+    public void testAdd() {
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, calc.subtract(3, 2));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(6, calc.multiply(2, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calc.divide(6, 3));
+    }
+
+    @Test
+    public void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> calc.divide(1, 0));
+    }
+}
